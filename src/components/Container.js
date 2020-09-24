9ff91bf985c0040b011264b041615e77b20023dc
Eryk Malinowski <eryk.malinowski1992@gmail.com>
2020-09-24 14:41:41 +0200

    add logic to the searchbar (not completed)

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,14 +1,52 @@
-import React from "react";
-import Players from "./Players";
-import Searchbar from "./Searchbar";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "./Container.module.css";
+import PlayersContext from "../contexts/PlayersContext";
 
 export default function Container() {
+  const data = useContext(PlayersContext);
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  useEffect(() => {
+    const results = data.players.filter(
+      (player) =>
+        player.name.toLowerCase().includes(searchTerm) ||
+        player.club.toLowerCase().includes(searchTerm)
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+
   return (
     <div className={styles.container}>
-      <Searchbar />
-      <h2 className={styles.subtitle}>All Players</h2>
-      <Players />
+      <div className={styles.searchbar}>
+        <div className={styles.search}>
+          <span>Search For</span>
+          <input type="text" value={searchTerm} onChange={handleChange} />
+        </div>
+        <button className={styles.filters}>Filters</button>
+      </div>
+      <h2 className={styles.subtitle}>Players</h2>
+      <ul className={styles.players}>
+        {data.loading ? (
+          <li className={styles.player}>Loading...</li>
+        ) : searchResults.length > 0 ? (
+          searchResults.map((item) => (
+            <li key={item.id} className={styles.player}>
+              <div className={styles.player__name}>{item.name}</div>
+              <div className={styles.player__club}>{item.club}</div>
+              <div className={styles.player__position}>{item.position}</div>
+            </li>
+          ))
+        ) : (
+          <li className={styles.player}>
+            <div className={styles.player__name}>No players found</div>
+          </li>
+        )}
+      </ul>
     </div>
   );
 }
