d0de496a4894c058f7e1a4d841eb18b26a1b8b40
Eryk Malinowski <eryk.malinowski1992@gmail.com>
2020-09-24 10:16:43 +0200

    add basic styles to the searchbar

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Players from "./Players";
+import Searchbar from "./Searchbar";
 import styles from "./Container.module.css";
 
 export default function Container() {
   return (
     <div className={styles.container}>
+      <Searchbar />
       <h2 className={styles.subtitle}>All Players</h2>
       <Players />
     </div>
