11b5b8b1ca8cd6c902057041c091ddf134578847
Eryk Malinowski <eryk.malinowski1992@gmail.com>
2020-11-22 12:44:37 +0100

    modify radio buttons in the Filters component

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -6,30 +6,30 @@ export default function Filters(props) {
     
     return (
         <div className={styles.filters}>
-          <div className={styles.filter} onChange={handleAge}>
+          <div className={styles.filter}>
             <span>Age</span>
-            <input type="radio" value="0-18" name="age" id="age1" defaultChecked={filters.ageValues.minAge === 0 && filters.ageValues.maxAge === 18} />
+            <input type="radio" value="0-18" name="age" id="age1" checked={filters.ageValues.minAge === 0 && filters.ageValues.maxAge === 18} onChange={handleAge} />
             <label htmlFor="age1">0-18</label>
-            <input type="radio" value="19-25" name="age" id="age2" defaultChecked={filters.ageValues.minAge === 19 && filters.ageValues.maxAge === 25} />
+            <input type="radio" value="19-25" name="age" id="age2" checked={filters.ageValues.minAge === 19 && filters.ageValues.maxAge === 25} onChange={handleAge} />
             <label htmlFor="age2">19-25</label>
-            <input type="radio" value="26-32" name="age" id="age3" defaultChecked={filters.ageValues.minAge === 26 && filters.ageValues.maxAge === 32} />
+            <input type="radio" value="26-32" name="age" id="age3" checked={filters.ageValues.minAge === 26 && filters.ageValues.maxAge === 32} onChange={handleAge} />
             <label htmlFor="age3">26-32</label>
-            <input type="radio" value="33-99" name="age" id="age4" defaultChecked={filters.ageValues.minAge === 33 && filters.ageValues.maxAge === 99} />
+            <input type="radio" value="33-99" name="age" id="age4" checked={filters.ageValues.minAge === 33 && filters.ageValues.maxAge === 99} onChange={handleAge} />
             <label htmlFor="age4">33+</label>
-            <input type="radio" value="0-99" name="age" id="age5" defaultChecked={filters.ageValues.minAge === 0 && filters.ageValues.maxAge === 99} />
+            <input type="radio" value="0-99" name="age" id="age5" checked={filters.ageValues.minAge === 0 && filters.ageValues.maxAge === 99} onChange={handleAge} />
             <label htmlFor="age5">ALL</label>
           </div>
-          <div className={styles.filter} onChange={handlePosition}>
+          <div className={styles.filter}>
             <span>Position</span>
-            <input type="radio" value="GK" name="position" id="position1" defaultChecked={filters.positions === "GK"} />
+            <input type="radio" value="GK" name="position" id="position1" checked={filters.positions === "GK"} onChange={handlePosition} />
             <label htmlFor="position1">GK</label>
-            <input type="radio" value="Def" name="position" id="position2" defaultChecked={filters.positions === "SW/DR/DC/DL"} />
+            <input type="radio" value="Def" name="position" id="position2" checked={filters.positions === "SW/DR/DC/DL"} onChange={handlePosition} />
             <label htmlFor="position2">Def</label>
-            <input type="radio" value="Mid" name="position" id="position3" defaultChecked={filters.positions === "DML/DMC/DMR/ML/MC/MR/AMR/AML/AMC"} />
+            <input type="radio" value="Mid" name="position" id="position3" checked={filters.positions === "DML/DMC/DMR/ML/MC/MR/AMR/AML/AMC"} onChange={handlePosition} />
             <label htmlFor="position3">Mid</label>
-            <input type="radio" value="Att" name="position" id="position4" defaultChecked={filters.positions === "FL/FC/FR/SC"} />
+            <input type="radio" value="Att" name="position" id="position4" checked={filters.positions === "FL/FC/FR/SC"} onChange={handlePosition} />
             <label htmlFor="position4">Att</label>
-            <input type="radio" value="All" name="position" id="position5" defaultChecked={filters.positions === "GK/SW/DR/DC/DL/DML/DMC/DMR/ML/MC/MR/AMR/AML/AMC/FL/FC/FR/SC"} />
+            <input type="radio" value="All" name="position" id="position5" checked={filters.positions === "GK/SW/DR/DC/DL/DML/DMC/DMR/ML/MC/MR/AMR/AML/AMC/FL/FC/FR/SC"} onChange={handlePosition} />
             <label htmlFor="position5">ALL</label>
           </div>
         </div>
