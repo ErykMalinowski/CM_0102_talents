012f946ac3edd3e6063116de8ccb1beb3510568e
Eryk Malinowski <eryk.malinowski1992@gmail.com>
2020-12-17 10:56:17 +0100

    modify Filters component

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -3,34 +3,46 @@ import styles from "./style.module.css";
 
 export const Filters = (props) => {
     const { filters, handleAge, handlePosition } = props;
+
+    const ageFilters = [
+      {"id": "age1", "minAge": 0, "maxAge": 18, "value": "0-18", "desc": "0-18"},
+      {"id": "age2", "minAge": 19, "maxAge": 25, "value": "19-25", "desc": "19-25"},
+      {"id": "age3", "minAge": 26, "maxAge": 32, "value": "26-32", "desc": "26-32"},
+      {"id": "age4", "minAge": 33, "maxAge": 99, "value": "33-99", "desc": "33+"},
+      {"id": "age5", "minAge": 0, "maxAge": 99, "value": "0-99", "desc": "ALL"}
+    ]
+
+    const positionFilters = [
+      {"id": "position1", "positions": ['GK'], "value": "GK", "desc": "GK"},
+      {"id": "position2", "positions": ['SW','DR','DC','DL'], "value": "Def", "desc": "Def"},
+      {"id": "position3", "positions": ['DML','DMC','DMR','ML','MC','MR','AMR','AML','AMC'], "value": "Mid", "desc": "Mid"},
+      {"id": "position4", "positions": ['FL','FC','FR','SC'], "value": "Att", "desc": "Att"},
+      {"id": "position5", "positions": ['GK','SW','DR','DC','DL','DML','DMC','DMR','ML','MC','MR','AMR','AML','AMC','FL','FC','FR','SC'], "value": "All", "desc": "ALL"}
+    ]
     
     return (
         <div className={styles.filters}>
           <div className={styles.filter}>
             <span>Age</span>
-            <input type="radio" value="0-18" name="age" id="age1" checked={filters.ageValues.minAge === 0 && filters.ageValues.maxAge === 18} onChange={handleAge} />
-            <label htmlFor="age1">0-18</label>
-            <input type="radio" value="19-25" name="age" id="age2" checked={filters.ageValues.minAge === 19 && filters.ageValues.maxAge === 25} onChange={handleAge} />
-            <label htmlFor="age2">19-25</label>
-            <input type="radio" value="26-32" name="age" id="age3" checked={filters.ageValues.minAge === 26 && filters.ageValues.maxAge === 32} onChange={handleAge} />
-            <label htmlFor="age3">26-32</label>
-            <input type="radio" value="33-99" name="age" id="age4" checked={filters.ageValues.minAge === 33 && filters.ageValues.maxAge === 99} onChange={handleAge} />
-            <label htmlFor="age4">33+</label>
-            <input type="radio" value="0-99" name="age" id="age5" checked={filters.ageValues.minAge === 0 && filters.ageValues.maxAge === 99} onChange={handleAge} />
-            <label htmlFor="age5">ALL</label>
+
+            {ageFilters.map(ageFilter => (
+              <React.Fragment key={ageFilter.id}>
+                <input type="radio" value={ageFilter.value} name="age" id={ageFilter.id} checked={filters.ageValues.minAge === ageFilter.minAge && filters.ageValues.maxAge === ageFilter.maxAge} onChange={handleAge} />
+                <label htmlFor={ageFilter.id}>{ageFilter.desc}</label>
+              </React.Fragment>
+            ))}
+
           </div>
           <div className={styles.filter}>
             <span>Position</span>
-            <input type="radio" value="GK" name="position" id="position1" checked={JSON.stringify(filters.positions) === JSON.stringify(['GK'])} onChange={handlePosition} />
-            <label htmlFor="position1">GK</label>
-            <input type="radio" value="Def" name="position" id="position2" checked={JSON.stringify(filters.positions) === JSON.stringify(['SW','DR','DC','DL'])} onChange={handlePosition} />
-            <label htmlFor="position2">Def</label>
-            <input type="radio" value="Mid" name="position" id="position3" checked={JSON.stringify(filters.positions) === JSON.stringify(['DML','DMC','DMR','ML','MC','MR','AMR','AML','AMC'])} onChange={handlePosition} />
-            <label htmlFor="position3">Mid</label>
-            <input type="radio" value="Att" name="position" id="position4" checked={JSON.stringify(filters.positions) === JSON.stringify(['FL','FC','FR','SC'])} onChange={handlePosition} />
-            <label htmlFor="position4">Att</label>
-            <input type="radio" value="All" name="position" id="position5" checked={JSON.stringify(filters.positions) === JSON.stringify(['GK','SW','DR','DC','DL','DML','DMC','DMR','ML','MC','MR','AMR','AML','AMC','FL','FC','FR','SC'])} onChange={handlePosition} />
-            <label htmlFor="position5">ALL</label>
+
+            {positionFilters.map(positionFilter => (
+              <React.Fragment key={positionFilter.id}>
+                <input type="radio" value={positionFilter.value} name="position" id={positionFilter.id} checked={JSON.stringify(filters.positions) === JSON.stringify(positionFilter.positions)} onChange={handlePosition} />
+                <label htmlFor={positionFilter.id}>{positionFilter.desc}</label>
+              </React.Fragment>
+            ))}
+
           </div>
         </div>
     )
